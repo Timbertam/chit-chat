b8fb357cc5dc9ca78bd738cd3ddab101d4e8da4e
martin-carril <martinflyvola2@gmail.com>
2026-07-25 18:19:31 -0300

    Oopsies, fixed the preloader lol

--- ChitChat.js
+++ ChitChat.js
@@ -1,70 +1,70 @@
 function preload_image('Textures/Assets/480px-Skies_SKY1029.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Assets/480px-Skies_SKY1029.png';
 }
 
 function preload_image('Textures/Assets/480px-Skies_SKY159.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Assets/480px-Skies_SKY159.png';
 
 }function preload_image('Textures/Assets/sunny.gif') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Assets/sunny.gif';
 
 }function preload_image('Textures/Assets/480px-Skies_SKY16.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Assets/480px-Skies_SKY16.png';
 
 }function preload_image('Textures/Animated/D2IRO/NIGHT/SKY8301.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Animated/D2IRO/NIGHT/SKY8301.png';
 
 }function preload_image('Textures/Assets/Skies_sky98.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Assets/Skies_sky98.png';
 }
 
 function preload_image('Textures/Assets/Skies_SKY99.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Assets/Skies_SKY99.png';
 }
 
 function preload_image('Textures/Assets/Skies_SKY12.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Assets/Skies_SKY12.png';
 }
 
 function preload_image('Textures/Assets/Skies_SKY20.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Assets/Skies_SKY20.png';
 }
 
 function preload_image('Textures/Assets/SKY55.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Assets/SKY55.png';
 }
 
 function preload_image('Textures/Assets/SKY18.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Assets/SKY18.png';
 }
 
 function preload_image('Textures/Assets/SKY30.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Assets/SKY30.png';
 }
 
 function preload_image('Textures/Skies/SKY6278.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Skies/SKY6278.png';
 }
 
 function preload_image('Textures/Skies/SKY6288.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Skies/SKY6288.png';
 }
 
 function preload_image('Textures/Skies/SKY6351.png') {
   let img = new Image();
-  img.src = im_url;
+  img.src = 'Textures/Skies/SKY6351.png';
 }
